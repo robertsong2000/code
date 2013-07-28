93ade51991798ea482c456a55240520508f92354
ra <Song.Luo@sony.com.cn>
2013-07-28 14:47:45 +0800

    add a test example for multi-libraries with same symbols.

---
+++ C/lib-with-same-symbol/bar.c
@@ -0,0 +1,8 @@
+#include "bar.h"
+#include <stdio.h>
+
+int foo(void)
+{
+	printf("foo in bar.c\n");
+	return 0;
+}
---
+++ C/lib-with-same-symbol/foo.c
@@ -0,0 +1,8 @@
+#include "foo.h"
+#include <stdio.h>
+
+int foo(void)
+{
+	printf("foo in foo.c\n");
+	return 0;
+}
