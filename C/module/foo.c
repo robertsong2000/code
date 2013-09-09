a151a0ec3ba3f864ff91dae84a396a511a1ed499
ra <Song.Luo@sony.com.cn>
2013-09-09 09:59:22 +0800

    update module.

---
+++ C/module/foo.c
@@ -0,0 +1,8 @@
+#include <linux/init.h>
+#include <linux/module.h> 
+#include <linux/kernel.h>
+
+int foo(void)
+{
+    return 0;
+}
--- C/module/test.c
+++ C/module/test.c
@@ -4,11 +4,21 @@
 
 MODULE_LICENSE("Dual BSD/GPL");  
 
+extern int bar(void);
+
 static int hello_init(void)  
 {  
+    bar();
     printk(KERN_ALERT "hello,world.\n");  
     return 0;  
 }  
+
+int helloworld_test(void)
+{
+    return 0;
+}
+
+EXPORT_SYMBOL(helloworld_test);
 
 static void hello_exit(void)  
 {  
