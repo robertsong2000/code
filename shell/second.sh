e13b67de5aff49b18cb9128adf23b115eb5fe4dd
robertsong <robertsong.japan@gmail.com>
2013-09-11 00:09:40 +0800

    add script to test first/second script

---
+++ shell/second.sh
@@ -0,0 +1,11 @@
+function func1 {
+   fun=$1
+   book=$2
+   printf "fun=%s,book=%s\n" "${fun}" "${book}"
+}
+
+function func2 {
+   fun2=$1
+   book2=$2
+   printf "fun2=%s,book2=%s\n" "${fun2}" "${book2}"
+}
