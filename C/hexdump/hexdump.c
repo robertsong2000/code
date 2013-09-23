27b9fd14b4621eafb02e6db362e5ef9ad0bad4a1
Song.Luo <robertsong.japan@gmail.com>
2013-09-23 09:18:18 +0800

    add hexdump

---
+++ C/hexdump/hexdump.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+static void hex_dump(FILE *f, const unsigned char *buf, int size)
+{
+    int len, i, j, c;
+
+    for(i=0;i<size;i+=16) {
+        len = size - i;
+        if (len > 16)
+            len = 16;
+        fprintf(f, "%08x ", i);
+        for(j=0;j<16;j++) {
+            if (j < len)
+                fprintf(f, " %02x", buf[i+j]);
+            else
+                fprintf(f, "   ");
+        }
+        fprintf(f, " ");
+        for(j=0;j<len;j++) {
+            c = buf[i+j];
+            if (c < ' ' || c > '~')
+                c = '.';
+            fprintf(f, "%c", c);
+        }
+        fprintf(f, "\n");
+    }
+}
+
+int main(void)
+{
+    hex_dump(stdout, "helloworld,robertsong", 100);
+    return 0;
+}
