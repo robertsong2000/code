9f2e2a31aa38b565644ae8b1d6d4bfc062192977
Song.Luo <robertsong.japan@gmail.com>
2013-09-27 13:03:07 +0800

    init

---
+++ shell/open-file.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+function raopen {
+    if [ -z "$1" ] ; then
+      echo "no file"
+    else
+      x="${1%%"."*}"
+      if [[ $x = $1 ]]; then
+        vi "$@"
+      else
+    len="${#x}"
+    strlen="${#1}"
+    leftlen="$(($strlen - $len + 1))"
+    subfix="${1:leftlen}"
+    if [ "$subfix" == "pdf" ]; then
+      mupdf "$@"
+    elif [ "$subfix" == "doc" ]; then
+      ooffice "$@"
+    else
+      vi "$@"
+    fi
+      fi
+    fi
+}
+
+raopen "test.pdf"
