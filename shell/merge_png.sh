e7ec0513dbdc3f8760d9a1ee9004b1ba2c2066ff
robertsong <robertsong@robertsong-OptiPlex-9020.(none)>
2014-11-14 12:03:59 +0800

    update

--- shell/merge_png.sh
+++ shell/merge_png.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+str="";
 count=0;
 for i in *.png; do
     echo $i;
     count=$(($count + 1));
+    str="$str $count.pdf";
     mudraw -F pdf -o $count.pdf "$i";
 done
-gs -q -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=final.pdf -dBATCH *.pdf
+gs -q -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=final.pdf -dBATCH $str
