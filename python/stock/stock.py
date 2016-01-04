2035b947bcb8ebc455b7d745d2611a5bebcb9a87
Song.Luo <Song.Luo@sony.com.cn>
2016-01-04 14:11:52 +0800

    Fix the warning info.

--- python/stock/stock.py
+++ python/stock/stock.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python
 # coding: UTF-8
 
+"""This script parse stock info"""
+
 import urllib
 
 def get_price(code):
-        url = "http://hq.sinajs.cn/?list=%s" % code
-        opener = urllib.FancyURLopener()
-        html = opener.open(url).read().decode('gbk')
-        data = html.split('"')[1].split(',')
-        name = "%-6s" %data[0]
-        price_current = "%-6s" % float(data[3])
-        change_percent = (float(data[3]) - float(data[2])) * 100 / float(data[2])
-        change_percent = "%-6s" % round (change_percent, 2)
-        print(u"股票名称:{0} 涨跌幅:{1}% 最新价:{2}元".format(name, change_percent, price_current))
+    '''print one stock info in details'''
+    url = "http://hq.sinajs.cn/?list=%s" % code
+    opener = urllib.FancyURLopener()
+    html = opener.open(url).read().decode('gbk')
+    data = html.split('"')[1].split(',')
+    name = "%-6s" % data[0]
+    price_current = "%-6s" % float(data[3])
+    change_percent = (float(data[3]) - float(data[2])) * 100 / float(data[2])
+    change_percent = "%-6s" % round (change_percent, 2)
+    print(u"股票名称:{0} 涨跌幅:{1}% 最新价:{2}元".format(name,
+        change_percent, price_current))
 
 def get_all_price(code_list):
+    '''process all stock'''
     for code in code_list:
         get_price(code)
 
-code_list = ['sh600219',           ##南山铝业
-             'sz000002',           ##万  科Ａ
-             'sz000623',           ##吉林敖东
-             'sz000725',           ##京东方Ａ
-             'sh600036',           ##招商银行
-             'sh601166',           ##兴业银行
-             'sh600298',           ##安琪酵母
-             'sh600881',           ##亚泰集团
-             'sz002582',           ##好想你  
-             'sh600750',           ##江中药业
-             'sh601088',           ##中国神华
-             'sz000338',           ##潍柴动力
-             'sz000895',           ##双汇发展
-             'sz000792']           ##盐湖股份
-get_all_price(code_list)
+if __name__ == '__main__':
+    STOCK = ['sh600219',       ##南山铝业
+             'sz000002',       ##万  科Ａ
+             'sz000623',       ##吉林敖东
+             'sz000725',       ##京东方Ａ
+             'sh600036',       ##招商银行
+             'sh601166',       ##兴业银行
+             'sh600298',       ##安琪酵母
+             'sh600881',       ##亚泰集团
+             'sz002582',       ##好想你  
+             'sh600750',       ##江中药业
+             'sh601088',       ##中国神华
+             'sz000338',       ##潍柴动力
+             'sz000895',       ##双汇发展
+             'sz000792']       ##盐湖股份
+
+    get_all_price(STOCK)
